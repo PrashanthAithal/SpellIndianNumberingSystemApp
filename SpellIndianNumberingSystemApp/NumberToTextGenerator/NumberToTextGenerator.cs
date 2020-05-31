1c10d3f12d745522ae2864b7d1b345c88178d1cf
Prashanth Aithal <50479415+PrashanthAithal@users.noreply.github.com>
2020-05-31 16:24:26 +0530

    Addition of new class in console project.
    Addition of new class in console project.

---
+++ SpellIndianNumberingSystemApp/NumberToTextGenerator/NumberToTextGenerator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NumberToTextGenerator
+{
+    public static class NumberToTextGenerator
+    {
+        public static string NumberToWord(int num)
+        {
+           throw new NotimplimentedException();
+        }
+    }
+}
