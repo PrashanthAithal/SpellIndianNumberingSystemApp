6cc4759b23402ba53ccf32ea1e7c506b74161b99
Prashanth Aithal <50479415+PrashanthAithal@users.noreply.github.com>
2020-05-31 16:11:57 +0530

    Addition of test method.
    New test method added.

---
+++ SpellIndianNumberingSystemApp/NumberToTextGeneratorTests/NumberToTextGeneratorTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace NumberToTextGeneratorTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void NumberToWordTest()
+        {
+             const Int64 numberToanalyze = 89898;
+             
+        }
+    }
+}
